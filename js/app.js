a0b1c57f5a9b20c565a7af1547776f99620da29a
mrogach2350 <m.rogach@gmail.com>
2016-08-19 15:25:55 -0700

    initial commit and adding reset button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 // wait for the DOM to finish loading
+// Sanity Check
+console.log("Sanity Check")
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
