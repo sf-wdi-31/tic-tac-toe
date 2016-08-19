a850003093a9fd2c640196f059e0bb59dfd7675a
Lisa Olson <olson.lisa94@gmail.com>
2016-08-19 16:51:57 -0700

    Adds basic grid and tracks mouse movement on squareOne

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 // wait for the DOM to finish loading
+console.log("Hey I'm JavaScript");
+var paragraphs = $('p');
+var firstParagraph = paragraphs.eq(0);
+var resetBtn = $('.reset-btn');
+var spaceOne =
+// var turns
+// var
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+    // $('#reset').on('click', function () {
+      // $('#reset').button('reset') // button text will be "finished!"
+  for (var i = 0; i < board.eq(0); i++) {
+$('#spaceOne').on('click', function(spaceOneClick){
+
+    console.log(i);
+    $( "#spaceOne" ).last().addClass( "selectedOne" );
+}
+})
 
 });
