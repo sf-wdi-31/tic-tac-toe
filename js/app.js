54f596b0eb710ab9a31390abd4477183b993ff35
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-19 18:01:46 -0700

    pseudocode of function logic

--- js/app.js
+++ js/app.js
@@ -20,3 +20,57 @@ $(document).ready(function() {
 //  });
 
 });
+
+//box is clicked//
+  $(".box").on("click", function handleClick() {
+    var boxClicked = $(this).attr("id");
+    //change this line to block another click and/or send x or o image//
+    console.log(boxClicked);
+  });
+
+//who's playing//
+var xPlayer = ???
+var oPlayer = ???
+
+//who's turn is it//
+  var takingTurns =
+    if the number of xPlayer moves < the number of oPlayer moves
+      it is xPlayer turn
+      if the number of xPlayer moves > the number of oPlayer moves
+      it is oPlayer turn
+      //x always starts//
+      else it is xPlayer turn
+
+//claim a box function//
+  var claimBox =
+    is the box empty
+    if it is empty
+    .append("box")
+    x || o
+
+//track turns function//
+  var xMoves =
+    start with 0
+    count x boxes
+    total x moves
+
+  var oMoves =
+    start with 0
+    count o boxes
+    total o moves
+
+//win//
+  var checkForWin =
+  if x || o == 1, 2, 3 || 4, 5, 6 || 7, 8, 9 || 1, 5, 9 || 3, 5, 7 || 1, 4, 7 || 2, 5, 8 || 3, 6, 9
+    it is a win
+  else takingTurns
+
+//reset everything function//
+  if (checkForWin) {
+    clearBoard
+  }
+
+//detect a draw//
+  if (1, 2, 3, 4, 5, 6, 7, 8, 9) && !(checkforWin) {
+    clearBoard
+  }
