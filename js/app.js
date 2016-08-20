9b14cfb6428c8511e63bfe4e511ea4d3e771c048
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-19 17:04:55 -0700

    creates html grid, box specific id, board styling

--- js/app.js
+++ js/app.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+// testing jQuery linked
+//  $(window).on("load", function handleLoad(event) {
+//    alert("Your game is ready!");
+//  });
+
+//  testing click on box
+//  $(".col-md-4").on("click", function handleClick(event) {
+//    alert("You clicked a box!");
+//  });
+
+//  testing a specific box is clicked
+//  $(".box").on("click", function handleClick() {
+//    var idValue = $(this).attr("id");
+//    console.log(idValue);
+//  });
+
 });
