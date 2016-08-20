d49c2b0ec9da2ecc4fa3ecf2631227443630926c
mrogach2350 <m.rogach@gmail.com>
2016-08-20 13:21:51 -0700

    adding winner check

--- js/app.js
+++ js/app.js
@@ -8,27 +8,101 @@ $(document).ready(function() {
   var player2 = '<img src="imgs/pirate.png" alt="" />'
   var currentPlayer = player1;
   var counter = 0;
+  var p1HeldBoxes = [];
+  var p2HeldBoxes = [];
 
   $('.box').on('click', function eventHandler() {
 
     counter ++;
     if (currentPlayer === player1){
       $(this).html(player1);
+      p1HeldBoxes.push($(this).attr('id'));
+      console.log(p1HeldBoxes);
       currentPlayer = player2;
       $('#currentPlayer').html("<h3>Current Playa: Pirate!</h3>");
     } else {
       $(this).html(player2)
+      p2HeldBoxes.push($(this).attr('id'));
+      console.log(p2HeldBoxes);
       $('#currentPlayer').html("<h3>Current Playa: Ninja!</h3>");
       currentPlayer = player1;
     }
-    if (counter > 3){
-      if ($('#a1').html()=== $('#a2').html() === $('#a3').html()){
-        alert("The Winner is " + currentPlayer);
-      }
-    }
+    isHorizontal();
+    isVertical();
+    isDiagonal();
   });
+
+  function isHorizontal() {
+    if ((p1HeldBoxes.includes('1') &&
+        p1HeldBoxes.includes('2') &&
+        p1HeldBoxes.includes('3')) ||
+        (p1HeldBoxes.includes('4') &&
+        p1HeldBoxes.includes('5') &&
+        p1HeldBoxes.includes('6'))||
+        (p1HeldBoxes.includes('7') &&
+        p1HeldBoxes.includes('8') &&
+        p1HeldBoxes.includes('9'))){
+          $('#currentPlayer').html("<h3>The Winner is Ninja!</h3>");
+        };
+    if ((p2HeldBoxes.includes('1') &&
+        p2HeldBoxes.includes('2') &&
+        p2HeldBoxes.includes('3')) ||
+        (p2HeldBoxes.includes('4') &&
+        p2HeldBoxes.includes('5') &&
+        p2HeldBoxes.includes('6'))||
+        (p2HeldBoxes.includes('7') &&
+        p2HeldBoxes.includes('8') &&
+        p2HeldBoxes.includes('9'))){
+          $('#currentPlayer').html("<h3>The Winner is Pirate!</h3>");
+        }
+  };
+  function isVertical (){
+    if ((p1HeldBoxes.includes('1') &&
+        p1HeldBoxes.includes('4') &&
+        p1HeldBoxes.includes('7')) ||
+        (p1HeldBoxes.includes('2') &&
+        p1HeldBoxes.includes('5') &&
+        p1HeldBoxes.includes('8'))||
+        (p1HeldBoxes.includes('3') &&
+        p1HeldBoxes.includes('6') &&
+        p1HeldBoxes.includes('9'))){
+          $('#currentPlayer').html("<h3>The Winner is Ninja!</h3>");
+        };
+    if ((p2HeldBoxes.includes('1') &&
+        p2HeldBoxes.includes('4') &&
+        p2HeldBoxes.includes('7')) ||
+        (p2HeldBoxes.includes('2') &&
+        p2HeldBoxes.includes('5') &&
+        p2HeldBoxes.includes('8'))||
+        (p2HeldBoxes.includes('3') &&
+        p2HeldBoxes.includes('6') &&
+        p2HeldBoxes.includes('9'))){
+          $('#currentPlayer').html("<h3>The Winner is Pirate!</h3>");
+        }
+  };
+  function isDiagonal(){
+    if ((p1HeldBoxes.includes('1') &&
+        p1HeldBoxes.includes('5') &&
+        p1HeldBoxes.includes('9')) ||
+        (p1HeldBoxes.includes('3') &&
+        p1HeldBoxes.includes('5') &&
+        p1HeldBoxes.includes('7'))){
+          $('#currentPlayer').html("<h3>The Winner is Ninja!</h3>");
+        };
+    if ((p2HeldBoxes.includes('1') &&
+        p2HeldBoxes.includes('5') &&
+        p2HeldBoxes.includes('9')) ||
+        (p2HeldBoxes.includes('3') &&
+        p2HeldBoxes.includes('5') &&
+        p2HeldBoxes.includes('7'))){
+          $('#currentPlayer').html("<h3>The Winner is Pirate!</h3>");
+        }
+  };
+
   $('.btn').on('click', function eventHandler(){
     $(".box").html(' ');
+    p1HeldBoxes = [];
+    p2HeldBoxes = [];
     currentPlayer = player1;
     $('#currentPlayer').html("<h3>Current Playa: Ninja!</h3>");
     counter = 0;
