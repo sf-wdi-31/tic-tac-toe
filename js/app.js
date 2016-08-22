40358c3ad9f12d42495824b678ffa1f72205b1d9
Scott Whitman <scott.w.whitman@gmail.com>
2016-08-21 17:27:16 -0700

    final version of tic-tac-toe

--- js/app.js
+++ js/app.js
@@ -3,16 +3,24 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  var boxClone = $('.box').each(function(element){
+    $('.box').clone();
+  })
+
   var playerX = 1;
   var playerO = 4;
   var playerUp = 1;
   var turnCount = 0;
 
-  if (playerUp === 1) {
-    $('#messageLine').text("Player X is up");
-  } else {
-    $('#messageLine').text("Player O is up");
+  var beginningMessage = function () {
+    if (playerUp === 1) {
+      $('#messageLine').text("Player X is up");
+    } else {
+      $('#messageLine').text("Player O is up");
+    };
   };
+
+  beginningMessage();
 
   var topArray = [0, 0, 0];
   var middleArray = [0, 0, 0];
@@ -24,53 +32,82 @@ $(document).ready(function(){
   var diagRightArray = [0, 0, 0];
   var fullArray = [topArray, middleArray, bottomArray, leftArray, centerArray, rightArray, diagLeftArray, diagRightArray]
 
-  $(".box").on("click", function designate(event) {
-    turnCount += 1;
-    var clickedBox = $(this);
-    if (playerUp === 1) {
-      var addCardX = $('<img src="x.png" class="box cardX">');
-      $(clickedBox).append(addCardX);
-    } else {
-      var addCardO = $('<img src="o.png" class="box cardO">');
-      $(clickedBox).append(addCardO);
-    };
-    $(clickedBox).off("click");
-    var boxID = $(clickedBox).attr('id')
-    eval(boxID + "()");
+  boxClick_On();
 
-    for (var i = 0; i < fullArray.length; i++) {
-      var sum = fullArray[i].reduce(add, 0);
-      function add(a, b) {
-        return a + b;
+  function boxClick_On() {
+    $(".box").on("click", function designate(event) {
+      turnCount += 1;
+
+      var clickedBox = $(this);
+
+      if (playerUp === 1) {
+        var addCardX = $('<img src="x.png" class="box cardX">');
+        $(clickedBox).append(addCardX);
+      } else {
+        var addCardO = $('<img src="o.png" class="box cardO">');
+        $(clickedBox).append(addCardO);
       };
-      // console.log(sum);
-      if (sum === 3) {
-        $('#messageLine').text("Player X is the winner!");
-        $(".box").off("click");
-        return;
-      } else if (sum === 12) {
-        $('#messageLine').text("Player O is the winner!");
-        $(".box").off("click");
-        return;
+
+      $(clickedBox).off("click");
+
+      var boxID = $(clickedBox).attr('id')
+
+      eval(boxID + "()");
+
+      for (var i = 0; i < fullArray.length; i++) {
+        var sum = fullArray[i].reduce(add, 0);
+        function add(a, b) {
+          return a + b;
+        };
+        // console.log(sum);
+        if (sum === 3) {
+          $('#messageLine').text("Player X is the winner!");
+          $(".box").off("click");
+          return;
+        } else if (sum === 12) {
+          $('#messageLine').text("Player O is the winner!");
+          $(".box").off("click");
+          return;
+        }
       }
-    }
 
-    if (turnCount === 9) {
-      $('#messageLine').text("The game is a draw!");
-      $(".box").off("click");
-    } else if (playerUp === playerX) {
-        playerUp = playerO;
-        $('#messageLine').text("Player O is up");
-    } else {
-        playerUp = playerX;
-        $('#messageLine').text("Player X is up");
-    }
+      if (turnCount === 9) {
+        $('#messageLine').text("The game is a draw!");
 
+        $(".box").off("click");
+
+        } else if (playerUp === playerX) {
+          playerUp = playerO;
+          $('#messageLine').text("Player O is up");
+        } else {
+          playerUp = playerX;
+          $('#messageLine').text("Player X is up");
+        }
+
+      });
+    };
+
+  $("#playAgain").on("click", function designate(event) {
+
+    $('.box').each(function(element){
+      $('.box').replaceWith(boxClone);
+    });
+
+    boxClick_On();
+
+    playerUp = 1;
+
+    turnCount = 0;
+
+    beginningMessage();
+
+    for (var m = 0; m < fullArray.length; m++) {
+      for (var j = 0; j < 3; j++) {
+        fullArray[m][j] = 0;
+      }
+      // console.log(fullArray[m]);
+    };
   });
-  //
-  // $("#playAgain").on("click", function() {
-  //   $('html').replaceWith(DomTreeCopy);
-  // });
 
   var topLeft = function() {
     topArray[0] = playerUp;
