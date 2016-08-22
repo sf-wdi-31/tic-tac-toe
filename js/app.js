a2b2f37692855c7e8524ff1ff9c44c07029d1b78
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-21 21:24:31 -0700

    creates checkForWin function and fixes x and o array

--- js/app.js
+++ js/app.js
@@ -41,43 +41,41 @@ var play = 0;
       $(this).text("x");
       $(this).addClass("x");
       }
+    checkforWin();
     });
 
-//creates box array//
+//SUCCESS check for a win//
 
-oArray = [];
-xArray = [];
+  function checkforWin () {
 
-//SUCCESS IN CONSOLE check for a win//
-//  $("#board").each(".box", function checkforWin() {
+    var oArray = [];
+    var xArray = [];
 
-  $(".o").each(function (item)) {
-    oArray.push($(this).attr("id"));
+    $(".o").each(function (item) {
+      oArray.push($(this).attr("id"));
+    });
 
-  };
-
-//    for oArray ($(".o").each(function (item)) {
-//      oArray.push($(this).attr("id"));
-//    });
-
-  $(".x").each(function (item)) {
-    xArray.push($(this).attr("id"));
+    $(".x").each(function (item) {
+      xArray.push($(this).attr("id"));
+    });
 
   };
 
-  var threeInARow = [("1" && "2" && "3") || ("4" && "5" && "6") || ("7" && "8" && "9") || ("1" && "5" && "9") || ("3" && "5" && "7") || ("1" && "4" && "7") || ("2" && "5" && "8") || ("3" && "6" && "9")];
+//  var threeInARow = [("1" && "2" && "3") || ("4" && "5" && "6") || ("7" && "8" && "9") || ("1" && "5" && "9") || ("3" && "5" && "7") || ("1" && "4" && "7") || ("2" && "5" && "8") || ("3" && "6" && "9")];
 
-  $(".box").on("click", function handleClick() {
-    if ($(".x") == threeInARow) {
-      alert("Congratulations X! You won!");
-    }
-    else if ($(".o") == threeInARow) {
-      alert("Congratulations O! You won!");
-    }
-    else {
-      null;
-    }
-  });
+//  $(".box").on("click", function handleClick() {
+//    if ($(".x") == threeInARow) {
+//      alert("Congratulations X! You won!");
+//    };
+//    else if ($(".o") == threeInARow) {
+//      alert("Congratulations O! You won!");
+//    };
+//    else {
+//      null;
+//    }
+
+
+//}
 
 //SUCCESS play again button - reset board//
   $(".btn-default").on("click", function handleClick() {
