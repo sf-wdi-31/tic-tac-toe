d59eaa4c229ad84b9d08848c7be46c8c77edc76e
Lisa Olson <olson.lisa94@gmail.com>
2016-08-21 20:06:53 -0700

    Reset button resets the game board

--- js/app.js
+++ js/app.js
@@ -2,25 +2,32 @@
 console.log("Hey I'm JavaScript");
 
 $(document).ready(function() {
+
+  $('.btn-primary').on('click', function(event) {
+      $( ".box" ).removeClass('glyphicon glyphicon-fire');
+      $('.box').removeClass('glyphicon glyphicon-tint');
+      player = 1;
+  })
+
 var player = 1;
   $('.box').on('click', function(event) {
 
     var squareSelected = $(this);
 
     if(squareSelected.hasClass('glyphicon glyphicon-fire') || squareSelected.hasClass('glyphicon glyphicon-tint')) {
-      alert('This square is taken! Try another square.');
+      alert('This spot is taken! Try another square.');
   } else {
       if(player === 1) {
         squareSelected.addClass('glyphicon glyphicon-fire');
         if(checkifPlayerWon('glyphicon glyphicon-fire')) {
-          alert("Congrats! Player "+player+ " has won!");
+          alert("Nice job player "+player+ "!!  Rematch?");
         } else {
         player = 2;
         }
       } else {
         squareSelected.addClass('glyphicon glyphicon-tint');
         if(checkifPlayerWon('glyphicon glyphicon-tint')) {
-          alert("Congrats! Player "+player+ " has won!");
+          alert("Nice job player "+player+ "!! Rematch?");
         } else {
           player = 1;
       }
@@ -50,6 +57,4 @@ function checkifPlayerWon (symbol){
   return false;
 }
 }
-
-
 });
